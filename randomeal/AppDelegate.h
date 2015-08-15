163cd1e1c8bced5a2e47bc80c0962adf0ff21286
Matteo Sandrin <sandrin98@hotmail.it>
2015-08-15 10:08:15 +0200

    Initial Commit

---
+++ randomeal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  randomeal
+//
+//  Created by Matteo Sandrin on 15/08/15.
+//  Copyright © 2015 Matteo Sandrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ randomeal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  randomeal
+//
+//  Created by Matteo Sandrin on 15/08/15.
+//  Copyright © 2015 Matteo Sandrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
